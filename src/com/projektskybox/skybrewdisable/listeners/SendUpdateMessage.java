7fc695ac22283e1ef362910baaf6fe4be1c850d6
Skyla <52376203+Cryptic-Peonix@users.noreply.github.com>
2021-01-14 22:16:40 -0500

    Created SendUpdateMessage Class
    Sends messages regarding plugin and config updates to the server admins

---
+++ src/com/projektskybox/skybrewdisable/listeners/SendUpdateMessage.java
@@ -0,0 +1,35 @@
+package com.projektskybox.skybrewdisable.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+
+import com.projektskybox.skybrewdisable.P;
+
+public class SendUpdateMessage implements Listener {
+
+	Plugin plugin = P.getInstance();
+
+	@EventHandler
+	public void onOpJoinEvent(PlayerJoinEvent event) {
+		Player admin = event.getPlayer();
+
+		if (admin.isOp()) {
+			//tell admin to update plugin version if it is outdated
+			if (P.updateAvailable == true) {
+				admin.sendMessage(
+						ChatColor.GOLD + "Sklyla's Brewing Disabler is outdated! Downnload the new version here: "
+								+ "https://www.spigotmc.org/resources/skylas-brewing-disabler.87836/");
+			}
+			// tell admin to replace the config if the versions are mismatched
+			if (P.mismatchedConfig == true) {
+				admin.sendMessage(ChatColor.DARK_RED + "Mismatched Configs! Some features may not work properly! Please ask the server owner to update the config!");
+			}
+		}
+
+	}
+
+}
