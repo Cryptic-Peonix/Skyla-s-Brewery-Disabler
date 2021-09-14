b79b2be3185ca13e68d80b9c28c997efcbb0b328
Skyla <skylaclark094@gmail.com>
2021-09-14 13:18:33 -0400

    recompiled in java 8

--- src/com/projektskybox/skybrewdisable/listeners/LingerSplaListener.java
+++ src/com/projektskybox/skybrewdisable/listeners/LingerSplaListener.java
@@ -29,8 +29,11 @@ public class LingerSplaListener implements Listener {
 		boolean splashDelete = plugin.getConfig().getBoolean("splash-potions.delete-item");
 		boolean lingerDelete = plugin.getConfig().getBoolean("lingering-potions.delete-item");
 		boolean weaknessDisabled = plugin.getConfig().getBoolean("splash-potions.disable-weakness");
+
+		//TODO make these admin perms into permission nodes so other plugins can use them (1.5b)
 		boolean splashAdminOverride = plugin.getConfig().getBoolean("splash-potions.admin-override");
 		boolean lingerAdminOverride = plugin.getConfig().getBoolean("lingering-potions.admin-override");
+
 
 		Random random = new Random();
 
