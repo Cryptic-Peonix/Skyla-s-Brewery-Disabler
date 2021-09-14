e5df4260d6fc36d973e9510841e099ac610032d0
Skyla <skylaclark094@gmail.com>
2021-09-14 12:43:16 -0400

    Added admin overrides for splash and lingering pots

--- src/com/projektskybox/skybrewdisable/P.java
+++ src/com/projektskybox/skybrewdisable/P.java
@@ -1,115 +1,106 @@
 package com.projektskybox.skybrewdisable;
 
-import java.util.logging.Logger;
-
+import com.projektskybox.skybrewdisable.bstats.MetricsLite;
+import com.projektskybox.skybrewdisable.commands.*;
+import com.projektskybox.skybrewdisable.listeners.*;
+import com.projektskybox.skybrewdisable.util.UpdateChecker;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.projektskybox.skybrewdisable.bstats.MetricsLite;
-import com.projektskybox.skybrewdisable.commands.DisableBrew;
-import com.projektskybox.skybrewdisable.commands.DisableLinger;
-import com.projektskybox.skybrewdisable.commands.DisableSplash;
-import com.projektskybox.skybrewdisable.commands.PotionDrinking;
-import com.projektskybox.skybrewdisable.commands.ReloadCommand;
-import com.projektskybox.skybrewdisable.listeners.BrewListener;
-import com.projektskybox.skybrewdisable.listeners.LingerSplaListener;
-import com.projektskybox.skybrewdisable.listeners.PotionDrinkListener;
-import com.projektskybox.skybrewdisable.listeners.SendUpdateMessage;
-import com.projektskybox.skybrewdisable.listeners.SplashLingerDispenserListener;
-import com.projektskybox.skybrewdisable.util.UpdateChecker;
+import java.util.logging.Logger;
 
-public class P extends JavaPlugin{
-	
-	private static P instance;
-	
-	//True if an update is available, false if not
-	public static boolean updateAvailable = false;
-	public static String currentVersion;
-	public static String newestVersion;
-	
-	//True if the config is mismatched, false is not
-	public static boolean mismatchedConfig = false;
-	
-	//creates an instance to reference in other classes
-	public static P getInstance() {
-		return instance;
-	}
-	
-	/**
-	 * Gets the Instance
-	 * Creates a logger
-	 * Loads the config
-	 * Enables listeners and commands
-	 * Runs the update checker
-	 */
-	@Override
-	public void onEnable() {
-		instance = this;
-		Logger logger = this.getLogger();
-		this.saveDefaultConfig();
-		
-		System.out.println("[Skyla's Brewing Disabler] Start");
-		
-		@SuppressWarnings("unused")
-		MetricsLite metrics = new MetricsLite(this, 10009);
-		
-		this.enableListeners();
-		this.enableCommands();
-		
-		//Checks the spigot API to see if the plugin needs an update
-		new UpdateChecker(this, 87836).getVersion(version -> {
-			currentVersion = this.getDescription().getVersion();
-			newestVersion = version;
-			if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
-				logger.info("There is no update available");
-				updateAvailable = false;
-			} else {
-				logger.info("Update Available!");
-				updateAvailable = true;
-			}
-		});
-		
-		//The config version of the extracted config file
-		String configVersion = this.getConfig().getString("config-version");
-		String currentConfigVersion = "1.4";
-		if (!(configVersion.equalsIgnoreCase(currentConfigVersion))) {
-			logger.info("Mismatched Configs! Please replace your config file!");
-			mismatchedConfig = true;
-		}
-		
-	}
-	
-	/**
-	 * Saves the config, sends a stop message, unregisters all events/commands, and stops the plugin
-	 */
-	@Override
-	public void onDisable() {
-		this.saveDefaultConfig();
-		System.out.println("[Skyla's Brewing Disabler] Stop");
-		HandlerList.unregisterAll();
-		instance = null;
-		updateAvailable = false;
-	}
-	
-	/**
-	 * Turns on all of the plugin listeners
-	 */
-	public void enableListeners() {
-		getServer().getPluginManager().registerEvents(new BrewListener(), this);
-		getServer().getPluginManager().registerEvents(new SplashLingerDispenserListener(), this);
-		getServer().getPluginManager().registerEvents(new LingerSplaListener(), this);
-		getServer().getPluginManager().registerEvents(new PotionDrinkListener(), this);
-		getServer().getPluginManager().registerEvents(new SendUpdateMessage(), this);
-	}
-	
-	/**
-	 * Enables all of the plugin commands
-	 */
-	public void enableCommands() {
-		this.getCommand("enable-potion-making").setExecutor(new DisableBrew());
-		this.getCommand("skybrewdisable").setExecutor(new ReloadCommand());
-		this.getCommand("splash-potions").setExecutor(new DisableSplash());
-		this.getCommand("linger-potions").setExecutor(new DisableLinger());
-		this.getCommand("potion-drinking").setExecutor(new PotionDrinking());
-	}
+public class P extends JavaPlugin {
+
+    private static P instance;
+
+    //True if an update is available, false if not
+    public static boolean updateAvailable = false;
+    public static String currentVersion;
+    public static String newestVersion;
+
+    //True if the config is mismatched, false is not
+    public static boolean mismatchedConfig = false;
+
+    //creates an instance to reference in other classes
+    public static P getInstance() {
+        return instance;
+    }
+
+    /**
+     * Gets the Instance
+     * Creates a logger
+     * Loads the config
+     * Enables listeners and commands
+     * Runs the update checker
+     */
+    @Override
+    public void onEnable() {
+        instance = this;
+        Logger logger = this.getLogger();
+        this.saveDefaultConfig();
+
+        System.out.println("[Skyla's Brewing Disabler] Start");
+
+        @SuppressWarnings("unused")
+        MetricsLite metrics = new MetricsLite(this, 10009);
+
+        this.enableListeners();
+        this.enableCommands();
+
+        //Checks the spigot API to see if the plugin needs an update
+        new UpdateChecker(this, 87836).getVersion(version -> {
+            currentVersion = this.getDescription().getVersion();
+            newestVersion = version;
+            if (this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info("There is no update available");
+                updateAvailable = false;
+            } else {
+                logger.info("Update Available!");
+                updateAvailable = true;
+            }
+        });
+
+        //The config version of the extracted config file
+        String configVersion = this.getConfig().getString("config-version");
+        String currentConfigVersion = "1.5";
+        if (!(configVersion.equalsIgnoreCase(currentConfigVersion))) {
+            logger.info("Mismatched Configs! Please replace your config file!");
+            mismatchedConfig = true;
+        }
+
+    }
+
+    /**
+     * Saves the config, sends a stop message, unregisters all events/commands, and stops the plugin
+     */
+    @Override
+    public void onDisable() {
+        this.saveDefaultConfig();
+        System.out.println("[Skyla's Brewing Disabler] Stop");
+        HandlerList.unregisterAll();
+        instance = null;
+        updateAvailable = false;
+    }
+
+    /**
+     * Turns on all of the plugin listeners
+     */
+    public void enableListeners() {
+        getServer().getPluginManager().registerEvents(new BrewListener(), this);
+        getServer().getPluginManager().registerEvents(new SplashLingerDispenserListener(), this);
+        getServer().getPluginManager().registerEvents(new LingerSplaListener(), this);
+        getServer().getPluginManager().registerEvents(new PotionDrinkListener(), this);
+        getServer().getPluginManager().registerEvents(new SendUpdateMessage(), this);
+    }
+
+    /**
+     * Enables all of the plugin commands
+     */
+    public void enableCommands() {
+        this.getCommand("enable-potion-making").setExecutor(new DisableBrew());
+        this.getCommand("skybrewdisable").setExecutor(new ReloadCommand());
+        this.getCommand("splash-potions").setExecutor(new DisableSplash());
+        this.getCommand("linger-potions").setExecutor(new DisableLinger());
+        this.getCommand("potion-drinking").setExecutor(new PotionDrinking());
+    }
 }
--- src/com/projektskybox/skybrewdisable/listeners/LingerSplaListener.java
+++ src/com/projektskybox/skybrewdisable/listeners/LingerSplaListener.java
@@ -1,8 +1,5 @@
 package com.projektskybox.skybrewdisable.listeners;
 
-import java.util.List;
-import java.util.Random;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -14,6 +11,9 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.potion.PotionType;
+
+import java.util.List;
+import java.util.Random;
 
 //Author: Skyla
 public class LingerSplaListener implements Listener {
@@ -29,6 +29,8 @@ public class LingerSplaListener implements Listener {
 		boolean splashDelete = plugin.getConfig().getBoolean("splash-potions.delete-item");
 		boolean lingerDelete = plugin.getConfig().getBoolean("lingering-potions.delete-item");
 		boolean weaknessDisabled = plugin.getConfig().getBoolean("splash-potions.disable-weakness");
+		boolean splashAdminOverride = plugin.getConfig().getBoolean("splash-potions.admin-override");
+		boolean lingerAdminOverride = plugin.getConfig().getBoolean("lingering-potions.admin-override");
 
 		Random random = new Random();
 
@@ -46,6 +48,8 @@ public class LingerSplaListener implements Listener {
 			ItemStack isOff = event.getPlayer().getInventory().getItemInOffHand();
 			// Lingering
 			if (isMain.getType() == Material.LINGERING_POTION) {
+				if (lingerAdminOverride && event.getPlayer().isOp())
+					return;
 				if (lingerEnable == false) {
 					event.setCancelled(true);
 					event.getPlayer().sendMessage(lingerMessage);
@@ -57,6 +61,8 @@ public class LingerSplaListener implements Listener {
 					}
 				}
 			} else if (isMain.getType() == Material.SPLASH_POTION) {
+				if (splashAdminOverride && event.getPlayer().isOp())
+					return;
 				PotionMeta weakMainMeta = (PotionMeta) isMain.getItemMeta();
 				if (weaknessDisabled == false && weakMainMeta.getBasePotionData().getType() == PotionType.WEAKNESS) {
 					return;
@@ -71,6 +77,8 @@ public class LingerSplaListener implements Listener {
 					}
 				}
 			} else if (isOff.getType() == Material.LINGERING_POTION) {
+				if (lingerAdminOverride && event.getPlayer().isOp())
+					return;
 				if (lingerEnable == false) {
 					event.setCancelled(true);
 					event.getPlayer().sendMessage(lingerMessage);
@@ -82,6 +90,8 @@ public class LingerSplaListener implements Listener {
 					}
 				}
 			} else if (isOff.getType() == Material.SPLASH_POTION) {
+				if (splashAdminOverride && event.getPlayer().isOp())
+					return;
 				PotionMeta weakOffMeta = (PotionMeta) isOff.getItemMeta();
 				if (weaknessDisabled == false && weakOffMeta.getBasePotionData().getType() == PotionType.WEAKNESS) {
 					return;
